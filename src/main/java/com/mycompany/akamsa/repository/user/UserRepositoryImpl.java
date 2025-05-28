4ed0096d88a9dfffcb3bb7bdd278dc38979b64f9
farhanisty <farhannivtasija@gmail.com>
2025-05-28 21:21:23 +0700

    implements all UserRepository methods in UserRepositoryImpl

--- src/main/java/com/mycompany/akamsa/repository/user/UserRepositoryImpl.java
+++ src/main/java/com/mycompany/akamsa/repository/user/UserRepositoryImpl.java
@@ -6,18 +6,24 @@ package com.mycompany.akamsa.repository.user;
 
 import com.mycompany.akamsa.entity.User;
 import com.mycompany.akamsa.exception.DuplicateEntryException;
+import com.mycompany.akamsa.exception.EntityNotFoundException;
 import com.mycompany.akamsa.helper.Connector;
 import com.mycompany.akamsa.exception.RepositoryException;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author farhannivta
  */
 public class UserRepositoryImpl implements UserRepository {
-    private String tableName = "user";
+    private final String tableName = "user";
 
     @Override
     public boolean insert(User user) throws RepositoryException, DuplicateEntryException {
@@ -53,22 +59,134 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public List<User> getAll() throws RepositoryException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM " + this.tableName;
+        
+        try {
+            Statement st = Connector.getConnection().createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            
+            while(rs.next()) {
+                User user = new User();
+                
+                user.setId(rs.getInt("id"));
+                user.setUsername(rs.getString("username"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+                user.setIsAdmin(rs.getBoolean("is_admin"));
+                user.setIsVerified(rs.getBoolean("is_verified"));
+                user.setUpdatedAt(rs.getString("updated_at"));
+                user.setCreatedAt(rs.getString("created_at"));
+                
+                users.add(user);
+            }
+        } catch (SQLException ex) {
+            throw new RepositoryException(ex.getMessage(), ex);
+        }
+        
+        return users;
     }
 
     @Override
-    public User getById(int id) throws RepositoryException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public User getById(int id) throws RepositoryException, EntityNotFoundException {
+        PreparedStatement st = null;
+        User user = new User();
+        
+        String sql = "SELECT * FROM " + this.tableName + " WHERE id = ?";
+
+        
+        try {
+            st = Connector.getConnection().prepareStatement(sql);
+            
+            st.setInt(1, id);
+            
+            ResultSet rs = st.executeQuery();
+            
+            if(rs.next()) {
+                user.setId(rs.getInt("id"));
+                user.setUsername(rs.getString("username"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+                user.setIsAdmin(rs.getBoolean("is_admin"));
+                user.setIsVerified(rs.getBoolean("is_verified"));
+                user.setUpdatedAt(rs.getString("updated_at"));
+                user.setCreatedAt(rs.getString("created_at"));
+            } else {
+                throw new EntityNotFoundException("id not found", new Throwable());
+            }
+        } catch(SQLException e) {
+            throw new RepositoryException(e.getMessage(), e);
+        } finally {
+            try {
+                st.close();
+            } catch(SQLException e) {
+                throw new RepositoryException(e.getMessage(), e);
+            }
+        }
+        
+        return user;
     }
 
     @Override
     public boolean update(User user) throws RepositoryException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         PreparedStatement st = null;
+        boolean result = false;
+        
+        try {
+            String sql = "UPDATE " + this.tableName + " SET username = ?, name = ?, password = ?, is_admin = ?, is_verified = ?, updated_at = NOW() WHERE id = ?";
+            st = Connector.getConnection().prepareStatement(sql);
+            
+            st.setString(1, user.getUsername());
+            st.setString(2, user.getName());
+            st.setString(3, user.getPassword());
+            st.setBoolean(4, user.getIsAdmin());
+            st.setBoolean(5, user.getIsVerified());
+            st.setInt(6, user.getId());
+            
+            if(st.executeUpdate() > 0) {
+                result = true;
+            } else {
+                throw new EntityNotFoundException("user id not found", new Throwable());
+            }
+        } catch(SQLException e) {
+            throw new RepositoryException(e.getMessage(), e);
+        } finally {
+            try {
+                st.close();
+            } catch(SQLException e) {
+                throw new RepositoryException(e.getMessage(), e);
+            }
+        }
+        
+        return result;
     }
 
     @Override
     public boolean delete(int id) throws RepositoryException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        PreparedStatement st = null;
+        boolean result = false;
+        
+        try {
+            String sql = "DELETE FROM " + this.tableName + " WHERE id = ?";
+            st = Connector.getConnection().prepareStatement(sql);
+            
+            st.setInt(1, id);
+            
+            if(st.executeUpdate() > 0) {
+                result = true;
+            } else {
+                throw new EntityNotFoundException("user id not found", new Throwable());
+            }
+        } catch(SQLException e) {
+            throw new RepositoryException(e.getMessage(), e);
+        } finally {
+            try {
+                st.close();
+            } catch(SQLException e) {
+                throw new RepositoryException(e.getMessage(), e);
+            }
+        }
+        
+        return result;
     }
-    
 }
