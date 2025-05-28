6f1db16deb9f40207b4b03eba2ccf3169ded11a8
farhanisty <farhannivtasija@gmail.com>
2025-05-28 18:55:00 +0700

    create Connector class

---
+++ src/main/java/com/mycompany/akamsa/helper/Connector.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.akamsa.helper;
+
+import com.mycompany.akamsa.config.Database;
+import java.sql.*;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+
+/**
+ *
+ * @author farhannivta
+ */
+public class Connector {
+    private static Connection connection = null;
+    
+    public static Connection getConnection() throws SQLException {
+        if(Connector.connection == null) {
+            MysqlDataSource datasource = new MysqlDataSource();
+            
+            datasource.setServerName(Database.host);
+            datasource.setUser(Database.user);
+            datasource.setPassword(Database.password);
+            datasource.setDatabaseName(Database.name);
+            
+            try {
+                Connector.connection = datasource.getConnection();
+            } catch(SQLException e) {
+                throw e;
+            }
+        }
+        
+        return Connector.connection;
+    }
+}
