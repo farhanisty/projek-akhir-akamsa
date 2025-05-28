ffd35d6312ef25106acd99477041a4809ed59781
farhanisty <farhannivtasija@gmail.com>
2025-05-28 19:57:06 +0700

    create getter and setter user entity

--- src/main/java/com/mycompany/akamsa/entity/User.java
+++ src/main/java/com/mycompany/akamsa/entity/User.java
@@ -14,4 +14,44 @@ public class User extends AbstractEntity {
     private String password;
     private boolean isAdmin;
     private boolean isVerified;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public boolean isIsVerified() {
+        return isVerified;
+    }
+
+    public void setIsVerified(boolean isVerified) {
+        this.isVerified = isVerified;
+    }
 }
